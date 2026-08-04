8d8ac288cebee85f5c6e20f7dc8ec573d020c25c
ezequielbes <ezequieltdbeserra@gmail.com>
2026-08-04 18:02:26 -0300

    chore(mobile): add named icon constants for plans screens

---
+++ mobile/lib/theme/plan_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const IconData planDepositIcon = Icons.arrow_downward_rounded;
+const IconData planWithdrawIcon = Icons.arrow_upward_rounded;
+const IconData planSavingsIcon = Icons.savings_outlined;
+const IconData planSuggestIcon = Icons.auto_awesome_outlined;
+const IconData planSimulatorIcon = Icons.insights_outlined;
+const IconData planDragHandleIcon = Icons.drag_indicator;
+const IconData planEditIcon = Icons.edit_outlined;
+const IconData planRemoveIcon = Icons.close;
