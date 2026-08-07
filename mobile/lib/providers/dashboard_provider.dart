d5f5ffbec11fa934f48c43b77ffd19f21af216a8
ezequielbes <ezequieltdbeserra@gmail.com>
2026-08-07 13:44:44 -0300

    feat(mobile): add Riverpod providers for dashboard summary and timeline

---
+++ mobile/lib/providers/dashboard_provider.dart
@@ -0,0 +1,17 @@
+// mobile/lib/providers/dashboard_provider.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile/providers/database_provider.dart';
+import 'package:mobile/providers/plans_provider.dart';
+import 'package:mobile/repositories/dashboard_repository.dart';
+
+final dashboardRepositoryProvider = Provider<DashboardRepository>((ref) {
+  return DashboardRepository(ref.watch(appDatabaseProvider), ref.watch(plansRepositoryProvider));
+});
+
+final dashboardSummaryProvider = FutureProvider<DashboardSummary>((ref) {
+  return ref.watch(dashboardRepositoryProvider).getSummary();
+});
+
+final dashboardTimelineProvider = FutureProvider<List<TimelineEvent>>((ref) {
+  return ref.watch(dashboardRepositoryProvider).getTimeline();
+});
